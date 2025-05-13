3f7fe98b448dfe12b35ecd590ed27e906443b77a
sahil-singh1107 <sahilpython@gmail.com>
2025-05-13 12:31:28 +0530

    added hero-banner

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,8 +1,10 @@
 import Navbar from "@/components/home/navbar";
+import HeroBanner from "@/components/home/hero-banner";
 
 const Page = () => {
     return <div>
         <Navbar/>
+        <HeroBanner/>
     </div>
 }
 
---
+++ src/components/home/hero-banner/index.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import {ArrowRight} from "lucide-react";
+import Image from "next/image";
+
+const HeroBanner = () => {
+    return <div className={"w-full mt-32 flex flex-col items-center justify-center"}>
+        <Image src={"/squiggles.png"} alt={"squiggles"} width={100} height={100} className={"absolute z-10 left-30 top-[190px]"}/>
+        <h1 className={"relative mb-12 leading-none -tracking-[2px] text-black text-center text-[65px] font-[1000]"}>The simplest way to create forms</h1>
+        <Image src={"/title-highlight-2.png"} alt={"title-highlight"} width={200} height={200}/>
+        <p className={"mb-14 text-center max-w-[600px] text-gray-8 font-[600]"}>Say goodbye to boring forms. Meet Tally — the free, intuitive form builder you’ve been looking for.</p>
+        <div className={"flex flex-col gap-y-2"}>
+            <a className={"text-white bg-blue-500 hover:bg-blue-800 font-[600] pt-1 pr-[0.7em] pb-1 pl-[0.7em] text-[14px] rounded-[7px]  flex items-center justify-center gap-x-[0.6em] gap-y-[0.6em] transition duration-200"}>
+                <p>Create a free form</p>
+                <ArrowRight className={"w-4 h-4"} />
+            </a>
+            <p className={"text-gray-7 text-[13px] text-center"}>No signup required</p>
+        </div>
+    </div>
+}
+
+export default HeroBanner
